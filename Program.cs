f48458faddfb957bb5d830371a91768ce770cfbb
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-12 13:37:48 -0600

    added instances for all vehicles

--- Program.cs
+++ Program.cs
@@ -7,13 +7,35 @@ namespace CustomColorsAndSounds
     {
         static void Main(string[] args)
         {
-            Zero fxs = new Zero();
-            Tesla modelS = new Tesla();
-            Cessna mx410 = new Cessna();
+            Zero fxs = new Zero()
+            {
+                BatteryKWh = 4.5,
+                MainColor = "grey",
+                MaximumOccupancy = "two",
+            };
+            Tesla modelS = new Tesla()
+            {
+                BatteryKWh = 5.0,
+                MainColor = "black",
+                MaximumOccupancy = "four",
+            };
+            Cessna mx410 = new Cessna()
+            {
+                FuelCapacity = 22.5,
+                MainColor = "dark blue",
+                MaximumOccupancy = "six",
+            };
+            Ram trx1500 = new Ram()
+            {
+                FuelCapacity = 22.5,
+                MainColor = "white",
+                MaximumOccupancy = "four",
+            };
 
             fxs.Drive();
             modelS.Drive();
             mx410.Drive();
+            trx1500.Drive();
         }
     }
 }
